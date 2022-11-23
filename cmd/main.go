89db908e1627d3e66c046db99b53051424e4dfff
Danylo Khutorianskyi <myfalseakk@gmail.com>
2022-11-23 19:09:51 +0200

    added pretty print

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"course_work_parallel_computing/indexer/blendedIndexer"
+)
+
 func main() {
 	//threadCount := 34
 
+	index, err := blendedIndexer.BuildIndex("./test/", 2)
+	if err != nil {
+		return
+	}
+
+	index.Print()
 }
